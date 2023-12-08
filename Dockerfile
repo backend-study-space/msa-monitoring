240c2e86e57dd4e9fc503a61717b0d8f76e313dc
2tsumo-hitori <audghks1996@naver.com>
2023-12-08 16:49:36 +0900

    모니터링 설정 수정

--- Dockerfile
+++ Dockerfile
@@ -15,4 +15,4 @@ EXPOSE 8080
 
 # Run the app
 #CMD ["java", "-javaagent:./otelagent/opentelemetry-javaagent.jar", "-jar", "./build/libs/demo-0.0.1-SNAPSHOT.jar"]
-CMD ["java", "-jar", "./build/libs/demo-0.0.1-SNAPSHOT.jar"]
+CMD ["java", "-jar", "./build/libs/getlog-0.0.1-SNAPSHOT.jar"]
--- src/main/kotlin/com/nt/getlog/config/PrometheusExemplarSamplerConfiguration.kt
+++ src/main/kotlin/com/nt/getlog/config/PrometheusExemplarSamplerConfiguration.kt
@@ -1,7 +1,11 @@
 package com.nt.getlog.config
 
-//@Configuration
-//class PrometheusExemplarSamplerConfiguration {
-//    @Bean
-//    fun openTelemetryAgentSpanContextSupplier() = OpenTelemetryAgentSpanContextSupplier();
-//}
+import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class PrometheusExemplarSamplerConfiguration {
+    @Bean
+    fun openTelemetryAgentSpanContextSupplier() = OpenTelemetryAgentSpanContextSupplier();
+}
